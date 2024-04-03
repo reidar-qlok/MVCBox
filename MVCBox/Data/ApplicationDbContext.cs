424a592427439b32aba40d8eab5902a28da18cdc
Reidar Nilsen <4reidar@gmail.com>
2024-04-03 22:01:15 +0200

    Add project files.

---
+++ MVCBox/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MVCBox.Models;
+
+namespace MVCBox.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ MVCBox/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCBox.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        [Required(ErrorMessage = "Description is requiered")]
+        public string MovieDescription { get; set; }
+    }
+}
